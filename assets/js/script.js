65ac5bcb10fd78feb67e87447fa6f99e7068a2c5
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2022-03-31 20:03:24 +0200

    Initialise roll action with die 1 condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,94 @@
-$(window).on('load', function() {
 
-  let images = [
-    "../img/die-1.svg",
-    "../img/die-2.svg",
-    "../img/die-3.svg",
-    "../img/die-4.svg",
-    "../img/die-5.svg",
-    "../img/die-6.svg"
-  ]
 
-  // Set variables
-  const btnNewGame = $('.content-new-game');
-  const btnRollDie = $('.content-roll-die');
-  const btnHold = $('.number-hold');
-  const dieImg = $('.die-img');
+// Set variables
+let images = [
+  "../img/die-1.svg",
+  "../img/die-2.svg",
+  "../img/die-3.svg",
+  "../img/die-4.svg",
+  "../img/die-5.svg",
+  "../img/die-6.svg"
+]
+const btnNewGame = document.querySelector('.content-new-game');
+const btnRollDie = document.querySelector('.content-roll-die');
+const btnHold = document.querySelector('.number-hold');
+const dieImg = document.querySelector('.die-img');
 
-  // Player 1
-  const numberPlayer1 = $('.number-player-1');
-  const numberCurrentPlayer1 = $('.number-current-1');
-  
-  // Player 1
-  const numberPlayer2 = $('.number-player-2');
-  const numberCurrentPlayer2 = $('.number-current-2');
 
-  // Function start new game
-  const newGame = function() {
-    numberPlayer1.text('0');
-    numberCurrentPlayer1.text('0');
-    numberPlayer2.text('0');
-    numberCurrentPlayer2.text('0');
-    
-  }
+// Player 1
+let textNumberPlayer1 = document.querySelector('.number-player-1');
+let textNumberCurrentPlayer1 = document.querySelector('.number-current-1');
 
-  // Function roll die
-  const rollDie = function() {
-    dieImg.addClass('roll');
+// Player 2
+let textNumberPlayer2 = document.querySelector('.number-player-2');
+let textNumberCurrentPlayer2 = document.querySelector('.number-current-2');
+
+// Players
+let players = document.querySelectorAll('.container-player');
+let textNumberCurrentPlayer = document.querySelector('.active .number-current');
+
+// Function start new game
+const newGame = function() {
+  textNumberPlayer1.innerHTML = "0";
+  textNumberCurrentPlayer1.innerHTML = "0";
+  textNumberPlayer2.innerHTML = "0";
+  textNumberCurrentPlayer2.innerHTML = "0";
+}
+
+// Function roll die
+const rollDie = function() {
+  let currentPlayer = document.querySelector('.active');
+  console.log(currentPlayer);
+  if (currentPlayer) {
+    let textNumberPlayer = document.querySelector('.active .number-player');
+    let numberPlayer = textNumberPlayer.textContent;
+    let newNumberPlayer = 0;
+    dieValue = Math.floor(Math.random() * 6)
+    dieImg.classList.add('roll');
+
     setTimeout(function() {
-      dieImg.removeClass('roll');
+      dieImg.classList.remove('roll');
+      dieImg.setAttribute("src", images[dieValue]);
+      
+      newNumberPlayer = parseInt(numberPlayer) + dieValue + 1;
+      console.log(dieValue);
+      if (dieValue == 0) {
+        newNumberPlayer = 0;
+        textNumberPlayer.innerHTML = "0";
+        players.forEach(function(player) {
+          if (player.classList.contains('active')) {
+            player.classList.remove('active');
+          } else {
+            player.classList.add('active');
+          }
+          
+        })
+      } else {
+        textNumberPlayer.innerHTML = newNumberPlayer;
 
-    }, 1100)
-  }
-
-  // Function hold
-  const hold = function() {
+        return newNumberPlayer;
+      }
+    }, 1100);
     
   }
+  delete(currentPlayer);
+}
 
-
+// Function hold
+const hold = function() {
   
-  // Start new game
-  btnNewGame.on('click', newGame);
-  
-  // Roll the die
-  btnRollDie.on('click', rollDie);
+}
 
-  // Hold
-  btnHold.on('click', hold);
 
-});
+
+// Start new game
+btnNewGame.addEventListener('click', newGame);
+
+// Roll the die
+btnRollDie.addEventListener('click', rollDie);
+
+// Hold
+/* btnHold.addEventListener('click', hold); */
+
 
 
